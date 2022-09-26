202de0a2cc330353449fff4421bca6e7778bba12
RDxu0213 <17606184601@163.com>
2022-09-26 18:35:33 +0800

    登录页面基本

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#go2Reg').on('click',function(){
+        $('.login-wrap').hide()
+        $('.reg-wrap').show()
+    })
+    $('#go2Login').on('click',function(){
+        $('.reg-wrap').hide()
+        $('.login-wrap').show()
+        
+    })
+})
